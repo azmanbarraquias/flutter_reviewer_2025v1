4c6ece74185c992da018f4c37f540c52edc0e2a0
Azman Barraquias <azmanbarraquias@gmail.com>
2025-02-25 12:43:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_reviewer_2025v1
+ This practice project serves as a collection of everything I've learned about Flutter development.
---
+++ android/app/src/main/kotlin/com/example/flutter_reviewer_2025v1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_reviewer_2025v1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
